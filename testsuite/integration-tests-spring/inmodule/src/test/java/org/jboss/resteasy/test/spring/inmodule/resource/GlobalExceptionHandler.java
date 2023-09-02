4f9f39de578e79e2ee80ed98ac8d7377dc612e2d
阿男 <l.weinan@gmail.com>
2023-09-03 00:22:39 +0800

    work

--- testsuite/integration-tests-spring/inmodule/src/test/java/org/jboss/resteasy/test/spring/inmodule/resource/GlobalExceptionHandler.java
+++ testsuite/integration-tests-spring/inmodule/src/test/java/org/jboss/resteasy/test/spring/inmodule/resource/GlobalExceptionHandler.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 //import org.springframework.web.context.annotation.ApplicationScope;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.NoHandlerFoundException;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.NoHandlerFoundException;
 
+
+//@EnableWebMvc
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -16,8 +19,9 @@ public class GlobalExceptionHandler {
 //        return new ResponseEntity<>("Error 404 - Page Not Found", HttpStatus.NOT_FOUND);
 //    }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)  // 409
-    @ExceptionHandler(NoHandlerFoundException.class)
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)  // 404
+    @ExceptionHandler(Throwable.class)
     public void handleConflict() {
         // Nothing to do
     }
