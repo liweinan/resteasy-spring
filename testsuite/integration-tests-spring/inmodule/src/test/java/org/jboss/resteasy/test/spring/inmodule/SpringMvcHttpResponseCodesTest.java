e0c0e8bedb1d450f33b5508025c5db1dad2467aa
阿男 <l.weinan@gmail.com>
2023-09-02 22:58:13 +0800

    strip test

--- testsuite/integration-tests-spring/inmodule/src/test/java/org/jboss/resteasy/test/spring/inmodule/SpringMvcHttpResponseCodesTest.java
+++ testsuite/integration-tests-spring/inmodule/src/test/java/org/jboss/resteasy/test/spring/inmodule/SpringMvcHttpResponseCodesTest.java
@@ -6,36 +6,37 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.jboss.arquillian.container.test.api.Deployment;
+//import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.as.arquillian.api.ServerSetup;
+//import org.jboss.arquillian.junit.Arquillian;
+//import org.jboss.as.arquillian.api.ServerSetup;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient43Engine;
-import org.jboss.resteasy.setup.AbstractUsersRolesSecurityDomainSetup;
+//import org.jboss.resteasy.setup.AbstractUsersRolesSecurityDomainSetup;
 import org.jboss.resteasy.spi.HttpResponseCodes;
-import org.jboss.resteasy.test.spring.inmodule.resource.SpringMvcHttpResponseCodesPerson;
-import org.jboss.resteasy.test.spring.inmodule.resource.SpringMvcHttpResponseCodesResource;
-import org.jboss.resteasy.test.spring.inmodule.resource.TestResource;
+//import org.jboss.resteasy.test.spring.inmodule.resource.SpringMvcHttpResponseCodesPerson;
+//import org.jboss.resteasy.test.spring.inmodule.resource.SpringMvcHttpResponseCodesResource;
+//import org.jboss.resteasy.test.spring.inmodule.resource.TestResource;
 import org.jboss.resteasy.utils.PortProviderUtil;
-import org.jboss.resteasy.utils.TestUtil;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.asset.StringAsset;
-import org.jboss.shrinkwrap.api.exporter.ZipExporter;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
+//import org.jboss.resteasy.utils.TestUtil;
+//import org.jboss.shrinkwrap.api.Archive;
+//import org.jboss.shrinkwrap.api.asset.StringAsset;
+//import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+//import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.core.MediaType;
+//import jakarta.ws.rs.client.Entity;
+//import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-import java.io.File;
+
+//import java.io.File;
 
 /**
  * @tpSubChapter Spring
@@ -43,30 +44,30 @@ import java.io.File;
  * @tpTestCaseDetails Tests various http response codes returned from the server
  * @tpSince RESTEasy 3.1.0
  */
-@ServerSetup({SpringMvcHttpResponseCodesTest.SecurityDomainSetup.class})
-@RunWith(Arquillian.class)
+//@ServerSetup({SpringMvcHttpResponseCodesTest.SecurityDomainSetup.class})
+//@RunWith(Arquillian.class)
 @RunAsClient
 public class SpringMvcHttpResponseCodesTest {
     private static Client authorizedClient;
     private static Client userAuthorizedClient;
     private static Client nonAutorizedClient;
 
-    @Deployment
-    public static Archive<?> deploy() {
-        WebArchive war = TestUtil.prepareArchive(SpringMvcHttpResponseCodesTest.class.getSimpleName());
-        war.addAsWebInfResource(SpringMvcHttpResponseCodesTest.class.getPackage(), "springMvcHttpResponseCodes/web-secure.xml", "web.xml");
-        war.addAsWebInfResource(SpringMvcHttpResponseCodesTest.class.getPackage(), "springMvcHttpResponseCodes/jboss-web.xml", "jboss-web.xml");
-        war.addAsWebInfResource(SpringMvcHttpResponseCodesTest.class.getPackage(), "springMvcHttpResponseCodes/mvc-dispatcher-servlet.xml", "mvc-dispatcher-servlet.xml");
-        war.addAsWebInfResource(SpringMvcHttpResponseCodesTest.class.getPackage(), "springMvcHttpResponseCodes/applicationContext.xml", "applicationContext.xml");
-        war.addAsManifestResource(new StringAsset("Dependencies: org.springframework.spring meta-inf\n"), "MANIFEST.MF");
-        war.addClass(SpringMvcHttpResponseCodesPerson.class);
-        Archive archive = TestUtil.finishContainerPrepare(war, null, SpringMvcHttpResponseCodesResource.class, TestResource.class);
-
-        archive.as(ZipExporter.class).exportTo(
-                new File("/tmp/SpringMvcHttpResponseCodesTest.war"), true);
-
-        return archive;
-    }
+//    @Deployment
+//    public static Archive<?> deploy() {
+//        WebArchive war = TestUtil.prepareArchive(SpringMvcHttpResponseCodesTest.class.getSimpleName());
+//        war.addAsWebInfResource(SpringMvcHttpResponseCodesTest.class.getPackage(), "springMvcHttpResponseCodes/web-secure.xml", "web.xml");
+//        war.addAsWebInfResource(SpringMvcHttpResponseCodesTest.class.getPackage(), "springMvcHttpResponseCodes/jboss-web.xml", "jboss-web.xml");
+//        war.addAsWebInfResource(SpringMvcHttpResponseCodesTest.class.getPackage(), "springMvcHttpResponseCodes/mvc-dispatcher-servlet.xml", "mvc-dispatcher-servlet.xml");
+//        war.addAsWebInfResource(SpringMvcHttpResponseCodesTest.class.getPackage(), "springMvcHttpResponseCodes/applicationContext.xml", "applicationContext.xml");
+//        war.addAsManifestResource(new StringAsset("Dependencies: org.springframework.spring meta-inf\n"), "MANIFEST.MF");
+//        war.addClass(SpringMvcHttpResponseCodesPerson.class);
+//        Archive archive = TestUtil.finishContainerPrepare(war, null, SpringMvcHttpResponseCodesResource.class, TestResource.class);
+//
+//        archive.as(ZipExporter.class).exportTo(
+//                new File("/tmp/SpringMvcHttpResponseCodesTest.war"), true);
+//
+//        return archive;
+//    }
 
     @Before
     public void init() {
@@ -105,17 +106,17 @@ public class SpringMvcHttpResponseCodesTest {
         return PortProviderUtil.generateURL(path, SpringMvcHttpResponseCodesTest.class.getSimpleName());
     }
 
-    /**
-     * @tpTestDetails Test server http response code for NotAcceptableException
-     * (The resource produces text/plain responses, while the client accepts application/json only)
-     * @tpSince RESTEasy 3.1.0
-     */
-    @Test
-    public void testNotAcceptableException() {
-        Response response = authorizedClient.target(generateURL("/" + TestResource.TEST_PATH)).request()
-                .accept(MediaType.APPLICATION_JSON_TYPE).get();
-        Assert.assertEquals(HttpResponseCodes.SC_NOT_ACCEPTABLE, response.getStatus());
-    }
+//    /**
+//     * @tpTestDetails Test server http response code for NotAcceptableException
+//     * (The resource produces text/plain responses, while the client accepts application/json only)
+//     * @tpSince RESTEasy 3.1.0
+//     */
+//    @Test
+//    public void testNotAcceptableException() {
+//        Response response = authorizedClient.target(generateURL("/" + TestResource.TEST_PATH)).request()
+//                .accept(MediaType.APPLICATION_JSON_TYPE).get();
+//        Assert.assertEquals(HttpResponseCodes.SC_NOT_ACCEPTABLE, response.getStatus());
+//    }
 
     /**
      * @tpTestDetails Test server http response code for NotFoundException
@@ -128,77 +129,77 @@ public class SpringMvcHttpResponseCodesTest {
         Assert.assertEquals(HttpResponseCodes.SC_NOT_FOUND, response.getStatus());
     }
 
-    /**
-     * @tpTestDetails Test server http response code for NotFoundException
-     * (The client sends a POST request to a resource path accepting GET only)
-     * @tpSince RESTEasy 3.1.0
-     */
-    @Test
-    public void testMethodNotAllowedException() {
-        Response response = authorizedClient.target(generateURL("/" + TestResource.TEST_PATH)).request().post(null);
-        Assert.assertEquals(HttpResponseCodes.SC_METHOD_NOT_ALLOWED, response.getStatus());
-    }
+//    /**
+//     * @tpTestDetails Test server http response code for NotFoundException
+//     * (The client sends a POST request to a resource path accepting GET only)
+//     * @tpSince RESTEasy 3.1.0
+//     */
+//    @Test
+//    public void testMethodNotAllowedException() {
+//        Response response = authorizedClient.target(generateURL("/" + TestResource.TEST_PATH)).request().post(null);
+//        Assert.assertEquals(HttpResponseCodes.SC_METHOD_NOT_ALLOWED, response.getStatus());
+//    }
+//
+//    /**
+//     * @tpTestDetails Test server http response code for BadRequestException
+//     * (The client sends a bad request, not matching expected data format)
+//     * @tpSince RESTEasy 3.1.0
+//     */
+//    @Test
+//    public void testBadRequestException() {
+//        Response response = authorizedClient.target(generateURL("/" + TestResource.TEST_PATH + "/json")).request()
+//                .post(Entity.entity("[{customer:\"Zack\"}]", MediaType.APPLICATION_JSON_TYPE));
+//        // As of RESTEasy 6.2 the default ExceptionMapper returns a 500 if the error is not handled. Previously, 400 was
+//        // returned.
+//        Assert.assertEquals(HttpResponseCodes.SC_INTERNAL_SERVER_ERROR, response.getStatus());
+//    }
+//
+//    /**
+//     * @tpTestDetails Test server http response code for NotSupportedException
+//     * (The client posts an application/xml request, while the server only accepts application/json
+//     * @tpSince RESTEasy 3.1.0
+//     */
+//    @Test
+//    public void testNotSupportedException() {
+//        Response response = authorizedClient.target(generateURL("/" + TestResource.TEST_PATH + "/json")).request()
+//                .post(Entity.entity("[{name:\"Zack\"}]", MediaType.APPLICATION_XML_TYPE));
+//        Assert.assertEquals(HttpResponseCodes.SC_UNSUPPORTED_MEDIA_TYPE, response.getStatus());
+//    }
+//
+//    /**
+//     * @tpTestDetails Test server http response code for NotAuthorizedException using client without credentials
+//     * @tpSince RESTEasy 3.1.0
+//     */
+//    @Test
+//    public void testNotAuthorizedException() {
+//        Response response = nonAutorizedClient.target(generateURL("/secured/json")).request()
+//                .post(Entity.entity("{\"name\":\"Zack\"}", MediaType.APPLICATION_JSON_TYPE));
+//        Assert.assertEquals(HttpResponseCodes.SC_UNAUTHORIZED, response.getStatus());
+//    }
+//
+//    /**
+//     * @tpTestDetails Test server http response code for ForbiddenException using client with credentials which are not
+//     * sufficient to grant access to the resource
+//     * @tpSince RESTEasy 3.1.0
+//     */
+//    @Test
+//    public void testForbiddenException() {
+//        Response response = userAuthorizedClient.target(generateURL("/secured/json")).request()
+//                .post(Entity.entity("{\"name\":\"Zack\"}", MediaType.APPLICATION_JSON_TYPE));
+//        Assert.assertEquals(HttpResponseCodes.SC_FORBIDDEN, response.getStatus());
+//    }
+//
+//    @Test
+//    public void testOK() {
+//        Response response = authorizedClient.target(generateURL("/secured/json")).request()
+//                .post(Entity.entity("{\"name\":\"Zack\"}", MediaType.APPLICATION_JSON_TYPE));
+//        Assert.assertEquals(HttpResponseCodes.SC_OK, response.getStatus());
+//    }
 
-    /**
-     * @tpTestDetails Test server http response code for BadRequestException
-     * (The client sends a bad request, not matching expected data format)
-     * @tpSince RESTEasy 3.1.0
-     */
-    @Test
-    public void testBadRequestException() {
-        Response response = authorizedClient.target(generateURL("/" + TestResource.TEST_PATH + "/json")).request()
-                .post(Entity.entity("[{customer:\"Zack\"}]", MediaType.APPLICATION_JSON_TYPE));
-        // As of RESTEasy 6.2 the default ExceptionMapper returns a 500 if the error is not handled. Previously, 400 was
-        // returned.
-        Assert.assertEquals(HttpResponseCodes.SC_INTERNAL_SERVER_ERROR, response.getStatus());
-    }
-
-    /**
-     * @tpTestDetails Test server http response code for NotSupportedException
-     * (The client posts an application/xml request, while the server only accepts application/json
-     * @tpSince RESTEasy 3.1.0
-     */
-    @Test
-    public void testNotSupportedException() {
-        Response response = authorizedClient.target(generateURL("/" + TestResource.TEST_PATH + "/json")).request()
-                .post(Entity.entity("[{name:\"Zack\"}]", MediaType.APPLICATION_XML_TYPE));
-        Assert.assertEquals(HttpResponseCodes.SC_UNSUPPORTED_MEDIA_TYPE, response.getStatus());
-    }
-
-    /**
-     * @tpTestDetails Test server http response code for NotAuthorizedException using client without credentials
-     * @tpSince RESTEasy 3.1.0
-     */
-    @Test
-    public void testNotAuthorizedException() {
-        Response response = nonAutorizedClient.target(generateURL("/secured/json")).request()
-                .post(Entity.entity("{\"name\":\"Zack\"}", MediaType.APPLICATION_JSON_TYPE));
-        Assert.assertEquals(HttpResponseCodes.SC_UNAUTHORIZED, response.getStatus());
-    }
-
-    /**
-     * @tpTestDetails Test server http response code for ForbiddenException using client with credentials which are not
-     * sufficient to grant access to the resource
-     * @tpSince RESTEasy 3.1.0
-     */
-    @Test
-    public void testForbiddenException() {
-        Response response = userAuthorizedClient.target(generateURL("/secured/json")).request()
-                .post(Entity.entity("{\"name\":\"Zack\"}", MediaType.APPLICATION_JSON_TYPE));
-        Assert.assertEquals(HttpResponseCodes.SC_FORBIDDEN, response.getStatus());
-    }
-
-    @Test
-    public void testOK() {
-        Response response = authorizedClient.target(generateURL("/secured/json")).request()
-                .post(Entity.entity("{\"name\":\"Zack\"}", MediaType.APPLICATION_JSON_TYPE));
-        Assert.assertEquals(HttpResponseCodes.SC_OK, response.getStatus());
-    }
-
-    static class SecurityDomainSetup extends AbstractUsersRolesSecurityDomainSetup {
-        SecurityDomainSetup() {
-            super(SpringMvcHttpResponseCodesTest.class.getResource("users.properties"),
-                    SpringMvcHttpResponseCodesTest.class.getResource("roles.properties"));
-        }
-    }
+//    static class SecurityDomainSetup extends AbstractUsersRolesSecurityDomainSetup {
+//        SecurityDomainSetup() {
+//            super(SpringMvcHttpResponseCodesTest.class.getResource("users.properties"),
+//                    SpringMvcHttpResponseCodesTest.class.getResource("roles.properties"));
+//        }
+//    }
 }
